9dc6e3eb2463e0545e9eb9fd522fd2c424fc33c2
NTGAYEON-PC\NTGAYEON <yyangel05@gmail.com>
2018-08-17 13:27:19 +0900

    180817 윤가연 - 팔로우기능 구현 80%

---
+++ src/main/java/com/kh/follow/FollowListModel.java
@@ -0,0 +1,43 @@
+package com.kh.follow;
+
+public class FollowListModel {
+
+	private int follow;
+	private int following;
+	private String nickname;
+	private String prof_img;
+	private int follow_to;
+	
+	
+	public int getFollow() {
+		return follow;
+	}
+	public void setFollow(int follow) {
+		this.follow = follow;
+	}
+	public int getFollowing() {
+		return following;
+	}
+	public void setFollowing(int following) {
+		this.following = following;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getProf_img() {
+		return prof_img;
+	}
+	public void setProf_img(String prof_img) {
+		this.prof_img = prof_img;
+	}
+	public int getFollow_to() {
+		return follow_to;
+	}
+	public void setFollow_to(int follow_to) {
+		this.follow_to = follow_to;
+	}
+	
+}
